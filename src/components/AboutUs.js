ceaf52cca16418ed6b2604bdef6be24a4bf439d3
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-06-04 15:33:03 +0000

    rwa

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -12,10 +12,10 @@ const AboutUs = () => {
         </div>
         <div className="col-md-6">
           <p className="description">
-            Aquo wants to make assets liquid. The day of illiquid assets is done. 
+            Aquo wants to make real world assets liquid. The day of illiquid assets is done. 
           </p>
           <p className="description">
-          Aquo is here to make all assets liquid, no more counting pennies while owning huge assets. $AQUO is coin for all people, making them investors, giving them a stakeholder status. $AQUO is fueled by investing power. 
+          Aquo is a ERC20 token powering a platform to make all real world assets liquid, no more counting pennies while owning huge assets. $AQUO is coin for all people, making them investors, giving them a stakeholder status. $AQUO is fueled by investing might.
           </p>
         </div>
       </div>
